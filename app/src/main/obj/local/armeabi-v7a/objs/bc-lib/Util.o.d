7edb345b472a206151e292685be355a096b3e70e
xueguoliang <xueguoliang@gmail.com>
2016-11-28 16:07:45 +0800

    aaaa

--- app/src/main/obj/local/armeabi-v7a/objs/bc-lib/Util.o.d
+++
@@ -1,5 +0,0 @@
-/Users/xueguoliang/repo/HC/app/src/main/obj/local/armeabi-v7a/objs/bc-lib/Util.o: \
-  /Users/xueguoliang/repo/HC/app/src/main/jni/Util.cpp \
-  /Users/xueguoliang/repo/HC/app/src/main/jni/Util.h
-
-/Users/xueguoliang/repo/HC/app/src/main/jni/Util.h:
--- app/src/main/obj/local/x86_64/objs/bc-lib/Util.o.d
+++
@@ -1,5 +0,0 @@
-/Users/xueguoliang/repo/HC/app/src/main/obj/local/x86_64/objs/bc-lib/Util.o: \
-  /Users/xueguoliang/repo/HC/app/src/main/jni/Util.cpp \
-  /Users/xueguoliang/repo/HC/app/src/main/jni/Util.h
-
-/Users/xueguoliang/repo/HC/app/src/main/jni/Util.h:
